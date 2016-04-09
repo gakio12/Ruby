d9967c17673a6f8cc3ba63277c949160112c5b0b
Alexander Hutchins <gakio12@gmail.com>
2016-04-09 16:59:09 -0600

    Added while loop comparison

---
+++ comparisons/whileLoop/While.java
@@ -0,0 +1,13 @@
+/**
+ *  Counts 0-9 and prints each number on a 
+ *  new line.
+ */
+public class While {
+    public static void main(String args[]) {
+        int i = 0;
+        while (i < 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ comparisons/whileLoop/while.rb
@@ -0,0 +1,7 @@
+# Counts 0-9 and prints each number on 
+# a new line.
+i = 0
+while i < 10 do
+    puts(i)
+    i += 1
+end
