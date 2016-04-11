2f77ab50f5239de7a4cb6dc69a6a5bad8e22a9e4
group4 <gakio12@gmail.com>
2016-04-11 17:26:59 -0600

    modified examples to have the parser, and fixed tpyos in uses.html

--- examples/parseExample/parser.rb
+++ examples/parseExample/parser.rb
@@ -1,7 +1,12 @@
+# parser.rb
+#
 # Simple text file parser that uses a regex to grab any line
 # that contains something that looks like an email address.
 # Only looks for an "@" symbol in any line.
 # Displays the match to std out.
+#
+# Run with ruby parser.rb
+
 def parse
 	File.open('sample.txt').each do |line|
 		if line.match(/^.+@.+$/)
